3521fe468831dc692d1da0c287d961d0955324ff
hilaolu <i@phyzait.moe>
2021-10-18 21:34:32 +0800

    update

--- quick-install/binary-neovim.fish
+++ quick-install/binary-neovim.fish
@@ -1,16 +1,24 @@
 pushd .
 
-mkdir -p /tmp/hilaolu/neovim
-cd /tmp/hilaolu/neovim
 
 set home_dir $HOME
+set current (set time (date '+%s'))
+set user (whoami)
+set working_dir /tmp/$user-$current
+
+
 mkdir -p $home_dir/.local/bin
+mkdir -p /tmp/trash
+
+mkdir -p $working_dir
+cd $working_dir 
 
 wget https://code.phyzait.moe/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz && \
 tar xvf nvim-linux64* && \
 rm *.gz && \
-mv nvim-linux64 $home_dir/.local/ && \
-ln -s $home_dir/.local/nvim-linux64/bin/nvim $home_dir/.local/bin/.
+mv nvim-linux64 $home_dir/.local/nvim -T && \
+[ -e "$home_dir/.local/nvim/bin/nvim"] || 
+ln -s $home_dir/.local/nvim/bin/nvim $home_dir/.local/bin/.
 
 popd
 
