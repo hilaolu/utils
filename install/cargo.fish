504817c4ca7a924ce3afbacedd1d0e0e382e30e3
hilaolu <i@phyzait.moe>
2021-10-19 23:51:01 +0800

    update

--- install/cargo.fish
+++ install/cargo.fish
@@ -1,5 +1,3 @@
-env RUSTUP_DIST_SERVER='https://mirrors.tuna.tsinghua.edu.cn/rustup rustup install stable'
-
 if not type -q 'cargo'
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 end
