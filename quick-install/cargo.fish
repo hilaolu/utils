76a7a438cf214717fda4a3b01135e4da1d65033f
hilaolu <i@phyzait.moe>
2021-10-19 20:46:22 +0800

    update

--- quick-install/cargo.fish
+++ quick-install/cargo.fish
@@ -1,1 +1,8 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if not type -q 'cargo'
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+end
+
+
+set DIR (dirname (status --current-filename))
+
+fish $DIR/update-cargo-index.fish
---
+++ quick-install/update-cargo-index.fish
@@ -0,0 +1,21 @@
+
+set cargo_conf $HOME/.cargo/config
+
+mkdir -p $HOME/.cargo
+
+touch $cargo_conf
+
+set result (cat $cargo_conf | grep 'source.crates-io')
+
+if test -z result
+set append "
+[source.crates-io]
+replace-with = 'phyzait'
+
+[source.phyzait]
+registry = "https://code.phyzait.moe/rust-lang/crates.io-index.git"
+"
+echo $append >> $cargo_conf
+
+end
+    
